d210147cf71ec1a7f13d83ecda4c4e3b62e93c14
Dongruixuan Li <dongruixuan@hotmail.com>
2018-04-16 14:38:23 +0800

    Add 'Connect' button,but have some bugs.

--- EncryptedChatClient/src/main/java/top/starcatmeow/chat/client/biz/Main.java
+++ EncryptedChatClient/src/main/java/top/starcatmeow/chat/client/biz/Main.java
@@ -1,5 +1,6 @@
 package top.starcatmeow.chat.client.biz;
 
+import sun.plugin2.message.Message;
 import top.starcatmeow.chat.client.ui.ChatClientUI;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -15,6 +16,7 @@ public class Main {
     static Socket socket = null;
     static JLabel label = null;
     static ChatClientUI ccui = null;
+    private static Thread messagehandlerThread = null;
 
     public static void main(String[] args) {
         try {
@@ -22,6 +24,7 @@ public class Main {
         } catch (Exception e) {
             //TODO exception
         }
+        UIManager.put("RootPane.setupButtonVisible", false);
         ccui = new ChatClientUI();
         ccui.setDefaultLookAndFeelDecorated(true);
         ccui.pack();
@@ -30,61 +33,92 @@ public class Main {
         ccui.setLocationRelativeTo(null);
         ccui.setTitle("聊天系统客户端");
         ccui.setVisible(true);
-
         label = ccui.getJl1();
-        label.setText("等待输入服务器信息");
-
-        boolean b1 = true;
-        while (b1) {
-            String ip = JOptionPane.showInputDialog("请输入IP：");
-            String port = JOptionPane.showInputDialog("请输入端口：");
-            label.setText("正在建立Socket连接");
-            socket = null;
-            try {
-                socket = new Socket(ip, new Integer(port));
-                b1 = false;
-                label.setText("Socket连接建立成功，正在从服务器取得密钥（1/5）");
-            } catch (IOException e) {
-                e.printStackTrace();
-                label.setText("连接建立失败，重新输入服务器信息");
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                label.setText("连接建立失败，重新输入服务器信息");
-            }
-        }
-
-        try {
-            Cert.getAESKey(socket);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        new Thread(new MessageHandler(socket)).start();
-
-        final JTextField jtf = ccui.getJtf1();
-        JButton jb = ccui.getJb1();
-        DataOutputStream dos = null;
-        try {
-            dos = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final DataOutputStream finalDos = dos;
-        jb.addActionListener(new ActionListener() {
-
+        label.setText("等待用户操作");
+        JButton jb1, jb2, jb3;
+        jb1 = ccui.getJb1();
+        jb2 = ccui.getJb2();
+        jb3 = ccui.getJb3();
+        jb2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (e.getActionCommand() == "发送") {
-                    if (!jtf.getText().equals("")) {
+                if (e.getActionCommand() == "连接服务器") {
+                    jb2.setEnabled(false);
+                    label.setText("等待输入服务器信息");
+
+                    boolean b1 = true;
+                    while (b1) {
+                        String ip = JOptionPane.showInputDialog("请输入IP：");
+
+                        String port = JOptionPane.showInputDialog("请输入端口：");
+                        label.setText("正在建立Socket连接");
+                        socket = null;
                         try {
-                            finalDos.writeUTF(AES.getInstance().encrypt(jtf.getText()));
-                            jtf.setText("");
+                            socket = new Socket(ip, new Integer(port));
+                            b1 = false;
+                            label.setText("Socket连接建立成功，正在从服务器取得密钥（1/5）");
                         } catch (IOException e1) {
                             e1.printStackTrace();
+                            label.setText("连接建立失败，请检查IP或端口是否输入正确");
+                        } catch (NumberFormatException e1) {
+                            e1.printStackTrace();
+                            label.setText("连接建立失败，请检查IP或端口是否输入正确");
                         }
                     }
+
+                    try {
+                        Cert.getAESKey(socket);
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+
+                    messagehandlerThread = new Thread(new MessageHandler(socket));
+                    messagehandlerThread.start();
+
+                    final JTextField jtf = ccui.getJtf1();
+
+                    DataOutputStream dos = null;
+                    try {
+                        dos = new DataOutputStream(socket.getOutputStream());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    final DataOutputStream finalDos = dos;
+                    jb1.setEnabled(true);
+                    jb2.setEnabled(true);
+                    jb2.setText("断开连接");
+                    jb1.addActionListener(new ActionListener() {
+
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if (e.getActionCommand() == "发送") {
+                                if (!jtf.getText().equals("")) {
+                                    try {
+                                        finalDos.writeUTF(AES.getInstance().encrypt(jtf.getText()));
+                                        jtf.setText("");
+                                    } catch (IOException e1) {
+                                        e1.printStackTrace();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                } else if (e.getActionCommand() == "断开连接") {
+                    messagehandlerThread.stop();
+                    jb1.setEnabled(false);
+                    jb2.setEnabled(false);
+
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        JOptionPane.showMessageDialog(null, "断开失败！请尝试重启客户端！");
+                        e1.printStackTrace();
+                    }
+                    jb2.setEnabled(true);
+                    jb2.setName("连接服务器");
                 }
             }
         });
+
     }
 }
--- EncryptedChatClient/src/main/java/top/starcatmeow/chat/client/ui/ChatClientUI.java
+++ EncryptedChatClient/src/main/java/top/starcatmeow/chat/client/ui/ChatClientUI.java
@@ -10,12 +10,13 @@ import java.awt.event.KeyListener;
  * Created by Dongruixuan Li on 2017/1/30.
  */
 public class ChatClientUI extends JFrame {
-    private JPanel jp1 = null, jp2 = null, jp3 = null, jp4 = null;
+    private JPanel jp1 = null, jp2 = null, jp3 = null, jp4 = null, jp5 = null, jp6 = null;
     private JLabel jl1 = null;
     private JTextArea jta1 = null;
     private JScrollPane jsp1 = null;
     private JTextField jtf1 = null;
-    private JButton jb1 = null;
+    private JButton jb1 = null, jb2 = null, jb3 = null;
+    private JCheckBox jcb1 = null;
 
     public ChatClientUI() {
         jl1 = new JLabel("");
@@ -46,10 +47,14 @@ public class ChatClientUI extends JFrame {
             }
         });
         jb1 = new JButton("发送");
+        jb2 = new JButton("连接服务器");
+        jb3 = new JButton("使用其它连接");
         jp1 = new JPanel();
         jp2 = new JPanel();
         jp3 = new JPanel();
         jp4 = new JPanel();
+        jp5 = new JPanel();
+        jp6 = new JPanel();
 
         jp4.setLayout(new BorderLayout(5, 5));
         jp4.add("Center", jtf1);
@@ -64,8 +69,17 @@ public class ChatClientUI extends JFrame {
         jp2.add(jl1);
         jp2.setBorder(BorderFactory.createTitledBorder("状态"));
 
+        jp5.setLayout(new BorderLayout(5, 5));
+        jp5.add("West", jb2);
+        jp5.add("East", jb3);
+        jp5.setBorder(BorderFactory.createTitledBorder("操作"));
+
+        jp6.setLayout(new BorderLayout(5, 5));
+        jp6.add("Center", jp2);
+        jp6.add("East", jp5);
+
         jp1.setLayout(new BorderLayout(0, 0));
-        jp1.add("North", jp2);
+        jp1.add("North", jp6);
         jp1.add("Center", jp3);
         setLayout(null);
         setContentPane(jp1);
@@ -85,5 +99,13 @@ public class ChatClientUI extends JFrame {
 
     public JButton getJb1() {
         return jb1;
+    }
+
+    public JButton getJb2() {
+        return jb2;
+    }
+
+    public JButton getJb3() {
+        return jb3;
     }
 }
